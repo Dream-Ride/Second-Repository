1b08f71df70c591590856eb925b7a2a1b5fe9225
Dream-Ride <103274522+Dream-Ride@users.noreply.github.com>
2022-10-07 14:19:46 -0300

    Create RSA.py

---
+++ RSA.py
@@ -0,0 +1,9 @@
+# Second-Repository
+from ast import For
+
+publica = 8383
+abc = open("abecedario.txt", mode="r")
+mensaje = input( " Escribe un mensaje para encriptar: " ).casefold
+
+
+numero = *27 +

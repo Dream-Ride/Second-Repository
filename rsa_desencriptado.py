ce44a2fb8b12e9b75e7f2ee581255d8c6cbc4403
Dream-Ride <103274522+Dream-Ride@users.noreply.github.com>
2022-10-22 18:57:48 -0300

    Update rsa_desencriptado.py

--- rsa_desencriptado.py
+++ rsa_desencriptado.py
@@ -1,57 +1,55 @@
 from ast import For
+import colorama 
+from colorama import Fore, Back
+colorama.init(autoreset=True)
 
 publica_1 = 8383
 d = 5467
 abc = [ "_" , "a", "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p", "q" , "r" , "s", "t" , "u" , "v" , "w" , "x" , "y" , "z"]
-b = input( " Escribe un mensaje para desencriptar: " ).casefold()
+b = input( "Bienvenido a la desencriptación de mensajes por el sistema RSA. Escriba el mensaje a desencriptar: " ).casefold()
 lista = []
 mensaje_cifrado = []
 
 for i in b:
     lista.append(i)
 
+if len(lista) % 3 != 0:
+    lista.append("_")
+
 for i in lista:
     for j in abc:
         if(i==j):
-            print(i)
             mensaje_cifrado.append(abc.index(i))
             break
-print(mensaje_cifrado)
 
 largo = len(mensaje_cifrado) - 1
-a = []
+mensaje = []
 
 for n in range(0, largo):
     if n % 3 == 0:
+        a = []
+        c = []
+        #pasaje de base 27
         texto_cifrado = int(mensaje_cifrado[n])*(27 ** 2) + int(mensaje_cifrado[n + 1])*27 + int(mensaje_cifrado[n + 2])
-        print(texto_cifrado)
+        #calculo del M = texto_llano
         texto_llano = (texto_cifrado ** d) % publica_1
-        print(texto_llano)
-        while texto_llano > 27:
-            cociente = int(texto_llano / 27)
-            print(cociente)
-            a.append(cociente)
-            resto = int(texto_llano % 27)
-            print(resto)
-            a.append(resto)
-print(a)
+        #pasaje a base 27
+        if texto_llano >= 27:
+            while texto_llano >= 27:
+                a.insert(0, texto_llano % 27)
+                texto_llano = int(texto_llano / 27)
+            a.insert(0, texto_llano)
+        else:
+            a.insert(0, texto_llano)
+            a.insert(0, 0)
 
-c = []
+        for i in a:
+            c.append(abc[i])
 
-for i in a:
-    c.append(abc[i])
+        for n in range(0, len(c) - 1):
+            mensaje.append(''.join(c[n : n + 2]))
 
-print(c)
-mensaje = []
+mensaje_final = ''.join(mensaje)
 
-while len(c) % 3 != 0:
-    c.append( '_')
-
-print(c)
-
-for i in range(0, len(c) - 1):
-    if i % 3 == 0:
-        mensaje.append(''.join(c[i : i + 3]))
-
-for i in range(len(mensaje)):
-    print(mensaje[i], end= "" )
+print( "\nSu mensaje es: ", end= "" )
+print(Fore.GREEN + mensaje_final.replace("_", " "), end= "" )

b9b3c2d052520df873eefab16d2b07d287c5c860
Dream-Ride <103274522+Dream-Ride@users.noreply.github.com>
2022-10-21 22:26:30 -0300

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,12 +1,16 @@
 from ast import For
+import colorama 
+from colorama import Fore, Back
+colorama.init(autoreset=True)
 
-publica = 8383
+publica_1 = 8383
+publica_2 = 3
 abc = [ "_" , "a", "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p", "q" , "r" , "s", "t" , "u" , "v" , "w" , "x" , "y" , "z"]
-mensaje = input( " Escribe un mensaje para encriptar: " )
+mensaje = input( " Bienvenido a la encriptacion de mensajes por el sistema RSA. Escribe en minuscula el mensaje a encriptar: " ).casefold()
 lista = []
 a = []
 
-for i in mensaje:
+for i in mensaje.replace(" ", "_"):
     lista.append(i)
 
 if len(lista) % 2 != 0:
@@ -15,31 +19,27 @@ if len(lista) % 2 != 0:
 for i in lista:
     for j in abc:
         if(i==j):
-            print(i)
             a.append(abc.index(i))
             break
-print(a)
 
-largo = len(a)-1
+largo = len(a) - 1
 mensaje_cifrado = []
 
 for n in range(0, largo):
     if n % 2 == 0:
         texto_llano = int(a[n])*27 + int(a[n + 1])
-        texto_cifrado = (texto_llano ** 3)%8383
+        texto_cifrado = (texto_llano ** publica_2) % publica_1
 
-        while texto_cifrado > 27:
+        while texto_cifrado >= 27:
             cifrado = texto_cifrado % 27
             mensaje_cifrado.insert(0, cifrado)
             texto_cifrado = int(texto_cifrado/27- cifrado/100)
         mensaje_cifrado.insert(0, texto_cifrado)
 
-print(mensaje_cifrado)
 b = []
 
 for i in mensaje_cifrado:
     b.append(abc[i])
-print(b)
 
 c = []
 
@@ -47,5 +47,6 @@ for i in range(0, len(b) - 1):
     if i % 3 == 0:
         c.insert(0, ''.join(b[i : i + 3]))
 
+print( "\n Su mensaje encriptado es: ", end= "" )
 for i in range(len(c)):
-    print(c[i], end= "" )
+    print(Fore.CYAN + c[i], end= "" )
